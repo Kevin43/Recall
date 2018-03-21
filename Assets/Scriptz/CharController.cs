8197d406a56b0fc3af16c187570d9a22f3e3f78d
Kevin43 <prekasstamos@gmail.com>
2018-03-21 14:23:08 -0400

    PostProcessing, Main Menu done
    Also added fps counter.

--- Assets/Scriptz/CharController.cs
+++ Assets/Scriptz/CharController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharController : MonoBehaviour {
 
@@ -24,6 +25,11 @@ public class CharController : MonoBehaviour {
         if (Input.GetButton("HorizontalKey") || Input.GetButton("VerticalKey"))
         {
             Move();
+        }
+        if (transform.position.y < -2)
+        {
+            //Respawn(); TODO: add respawn function
+            SceneManager.LoadScene(1);
         }
     }
 

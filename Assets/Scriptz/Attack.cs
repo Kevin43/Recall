bc78ab9f686147a90a859607f267ed8dc0a9c761
Kevin43 <prekasstamos@gmail.com>
2018-03-26 19:04:32 -0400

    first implement of attacking

---
+++ Assets/Scriptz/Attack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+    public float distance;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        RaycastHit hit;
+        if (Input.GetButtonDown("Attack"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, distance))
+            {
+                Debug.DrawRay(transform.position, transform.forward, Color.green);
+                print("Attack");
+                GameObject objectHit = hit.transform.gameObject;
+                AttackableObject actionObj = objectHit.GetComponent<AttackableObject>();
+                if (actionObj != null)
+                {
+                    actionObj.DoAction();
+                }
+            }
+            else
+            {
+                print("No attack");
+            }
+        }
+    }
+
+    void DebugHit()
+    {
+        print("Attack");
+    }
+}
---
+++ Assets/Scriptz/AttackableObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AttackableObject : MonoBehaviour {
+    public UnityEvent myUnityEvent2;
+
+	// Use this for initialization
+	void Awake () {
+        if (myUnityEvent2 == null)
+            myUnityEvent2 = new UnityEvent();
+    }
+
+    // Update is called once per frame
+    public void DoAction()
+    {
+        myUnityEvent2.Invoke();
+    }
+
+}

609bc2671b40c0292ce0770e965eecdfdbf63015
Kevin43 <prekasstamos@gmail.com>
2018-03-26 15:12:30 -0400

    First implementation of interaction
    Attack next

---
+++ Assets/Scriptz/Interact2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interact2 : MonoBehaviour {
+
+    public float distance;
+
+   // GameObject player = GameObject.Find("Player");
+    //public PlayerMovement playerMovement;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        RaycastHit hit;
+        if (Input.GetButtonDown("Interact"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, distance))
+            {
+                Debug.DrawRay(transform.position, transform.forward, Color.green);
+                print("Hit");
+                GameObject objectHit = hit.transform.gameObject;
+                InteractableObject actionObj = objectHit.GetComponent<InteractableObject>();
+                if (actionObj != null)
+                {
+                    actionObj.DoAction();
+                }
+            }
+            else
+            {
+                print("No hit");
+            }
+        }
+	} 
+    
+    void DebugHit()
+    {
+        print("hit");
+    }
+}
---
+++ Assets/Scriptz/InteractableObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractableObject : MonoBehaviour {
+    public UnityEvent myUnityEvent;
+	// Use this for initialization
+	void Awake () {
+        if (myUnityEvent == null)
+            myUnityEvent = new UnityEvent();
+	}
+	
+	// Update is called once per frame
+	public void DoAction () {
+        myUnityEvent.Invoke();
+	}
+}

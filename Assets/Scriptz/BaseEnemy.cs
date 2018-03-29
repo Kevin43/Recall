930df32c8b65d371cd497799e4eca595635a6172
Kevin43 <prekasstamos@gmail.com>
2018-03-28 20:04:28 -0400

    basis of enemy scripting

---
+++ Assets/Scriptz/BaseEnemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemy : MonoBehaviour {
+
+    protected int health;
+    protected int damage;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        if (health <= 0)
+        {
+            Die();
+        }
+
+	}
+
+    public void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scriptz/EnemyTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTest : BaseEnemy {
+
+    private int enemyHealth;
+    private string enemyName;
+
+
+	// Use this for initialization
+	void Start () {
+        enemyHealth = health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
